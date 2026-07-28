a1ffc7bcc9f8266b2c1363fcd2310a0fa36ec832
mataey <mataey@icloud.com>
2026-07-28 19:57:27 -0400

    Complete Week 4: Hooks and Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "Learn React" },
-    { id: 2, title: "Build a Todo App" },
-    { id: 3, title: "Submit assignment" }
-  ]);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+  }
 
   return (
     <main>
       <h1>CTD Swag Todo List</h1>
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </main>
   );
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,36 @@
-function TodoForm() {
+import React, { useState } from 'react';
+
+function TodoForm({ onAddTodo }) {
+  const [todoTitle, setTodoTitle] = useState('');
+
+  function handleTitleChange(event) {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  }
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    
+    onAddTodo({
+      title: todoTitle,
+      id: Date.now(),
+    });
+
+    setTodoTitle('');
+  }
+
   return (
-    <form>
-      <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>Add Todo</button>
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle">Title: </label>
+      <input
+        id="todoTitle"
+        name="title"
+        value={todoTitle}
+        onChange={handleTitleChange}
+      />
+      <button type="submit">Add</button>
     </form>
   );
 }
+
 export default TodoForm;
